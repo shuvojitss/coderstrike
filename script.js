41381261ef41357bfd9f46256566394fd2b2ce60
shuvojitss <142198651+shuvojitss@users.noreply.github.com>
2024-03-01 17:42:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-<script>
-window.embeddedChatbotConfig = {
-chatbotId: "qPLezrNGlXZukZJ7dUaJ6",
-domain: "www.chatbase.co"
-}
-</script>
-<script
-src="https://www.chatbase.co/embed.min.js"
-chatbotId="qPLezrNGlXZukZJ7dUaJ6"
-domain="www.chatbase.co"
-defer>
-</script>
+a
